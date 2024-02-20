f44e9e1dcd98f2f4c86c91b5ca2a34e682eacc0b
kaaaaakun_ <you@example.com>
2024-02-20 09:30:29 +0900

    fin ex00

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,9 +1,18 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <exception>
+#include <iostream>
+#include <list>
+#include <stdexcept>
+#include <vector>
+
 template <typename T>
-void easyfind(T &container, int value)
-{
+typename T::iterator easyfind(T &container, int value) {
+  typename T::iterator itr =
+      std::find(container.begin(), container.end(), value);
+  if (itr != container.end()) return itr;
+  throw std::invalid_argument("Value not found");
 }
 
-#endif // EASYFIND_HPP
+#endif  // EASYFIND_HPP
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,17 +1,60 @@
-#include "whatever.hpp"
+#include <deque>
+
+#include "easyfind.hpp"
 
 int main(void) {
-  int a = 2;
-  int b = 3;
-  ::swap(a, b);
-  std::cout << "a = " << a << ", b = " << b << std::endl;
-  std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
-  std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
-  std::string c = "chaine1";
-  std::string d = "chaine2";
-  ::swap(c, d);
-  std::cout << "c = " << c << ", d = " << d << std::endl;
-  std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
-  std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+  // vectorの場合
+  std::vector<int> vec;
+  for (int i = 0; i < 5; i++) {
+    vec.push_back(i);
+  }
+  try {
+    std::vector<int>::iterator itr = easyfind(vec, 3);
+    std::cout << *itr << std::endl;  // イテレータを解除して値を表示
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+  try {
+    std::vector<int>::iterator itr = easyfind(vec, 7);
+    std::cout << *itr << std::endl;  // イテレータを解除して値を表示
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+
+  // listの場合
+  std::list<int> list;
+  for (int i = 0; i < 5; i++) {
+    list.push_back(i);  // リストに要素を追加
+  }
+  try {
+    std::list<int>::iterator itr = easyfind(list, 3);
+    std::cout << *itr << std::endl;  // イテレータを解除して値を表示
+  } catch (const std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+  try {
+    std::list<int>::iterator itr = easyfind(list, 6);
+    std::cout << *itr << std::endl;  // イテレータを解除して値を表示
+  } catch (const std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+
+  // dequeの場合
+  std::deque<int> deq;
+  for (int i = 0; i < 5; i++) {
+    deq.push_back(i);
+  }
+  try {
+    std::deque<int>::iterator itr = easyfind(deq, 3);
+    std::cout << *itr << std::endl;
+  } catch (const std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+  try {
+    std::deque<int>::iterator itr = easyfind(deq, 10);
+    std::cout << *itr << std::endl;
+  } catch (const std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
   return 0;
 }
