a493c78bdfa04446ab848cd9a7bc97c936b5df7b
kaaaaakun <>
2024-02-27 20:18:33 +0900

    fix: なんか納得いっていなかったcontainer_typeの部分を直す

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -2,9 +2,10 @@
 #define MUTANTSTACK_HPP
 
 #include <stack>
+#include <deque>
 
 template <class Elem>
-class MutantStack : public std::stack<Elem> {
+class MutantStack : public std::stack<Elem, std::deque<Elem> > {
  public:
   // canonical form
   MutantStack() {}
@@ -19,24 +20,23 @@ class MutantStack : public std::stack<Elem> {
 
   // ---- begin and end and iterator ----
   // iterator
-  typedef typename std::stack<Elem>::container_type::iterator iterator;
+  typedef typename std::deque<Elem>::iterator iterator;
   iterator begin() { return (std::stack<Elem>::c.begin()); }
   iterator end() { return (std::stack<Elem>::c.end()); }
 
   // const
-  typedef
-      typename std::stack<Elem>::container_type::const_iterator const_iterator;
+  typedef typename std::deque<Elem>::const_iterator const_iterator;
   const_iterator begin() const { return (std::stack<Elem>::c.begin()); }
   const_iterator end() const { return (std::stack<Elem>::c.end()); }
 
   // reverse iterator
-  typedef typename std::stack<Elem>::container_type::reverse_iterator
+  typedef typename std::deque<Elem>::reverse_iterator
       reverse_iterator;
   reverse_iterator rbegin() { return (std::stack<Elem>::c.rbegin()); }
   reverse_iterator rend() { return (std::stack<Elem>::c.rend()); }
 
   // const reverse iterator
-  typedef typename std::stack<Elem>::container_type::const_reverse_iterator
+  typedef typename std::deque<Elem>::const_reverse_iterator 
       const_reverse_iterator;
   const_reverse_iterator rbegin() const {
     return (std::stack<Elem>::c.rbegin());
